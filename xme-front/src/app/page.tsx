7ba81ad5796a398bf44d37b56c45b8e29ab80d0d
Адема <adema@MacBook-Air-Adema.local>
2024-10-29 07:57:47 +0600

    xme-ended

---
+++ xme-front/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { LandingPageComponent } from "@/components/landing-page";
+
+
+export default function Home() {
+  return (
+    <div className="">
+      <LandingPageComponent />
+    </div>
+  );
+}
